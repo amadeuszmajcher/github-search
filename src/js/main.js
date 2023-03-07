6ab28938583ad190bf23c1ac1a038d0c6e583c2e
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2023-03-07 23:35:12 +0100

    js structure

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,28 @@
 
 // place your code below
 
+const list = document.querySelector('.list--js');
+const search = document.querySelector('.search--js');
+const buttonSearch = document.querySelector('.buttonSearch--js');
 
-console.log(`Hello world!`);
+buttonSearch.addEventListener('click', (e) => {
+    localStorage.setItem('name', search.value);
+    window.location.reload();
+});
+
+search.value = localStorage.getItem('name');
+const name = localStorage.getItem('name');
+
+fetch('https://api.github.com/users/'+name+'/repos')
+.then(resp => resp.json())
+.then(resp => {
+    const repos = resp;
+    for(const repo of repos){
+        list.innerHTML += `<li><a href="${repo.html_url}">${repo.name}</a></li>`;
+    }
+})
+.catch(err => {
+    console.log(err);
+})
 
 
